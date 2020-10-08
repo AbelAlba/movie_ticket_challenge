4f176186b1f993babf7ba383bd853ed5cdf8ade1
Abel Alba <amado.aac18@gmail.com>
2020-10-08 17:39:00 -0500

    Init Project

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:movie_ticket_challenge/src/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie Ticket Challenge',
+      localizationsDelegates: [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+ ],
+      supportedLocales: [
+          const Locale('en',''),
+          const Locale('es','ES')
+      ],
+      debugShowCheckedModeBanner: false,
+      initialRoute:'home',
+      routes: getApplicationRoutes(),
+    );
+  }
+  
+  Map<String, WidgetBuilder> getApplicationRoutes(){
+    return <String,WidgetBuilder> {
+          'home'       : ( BuildContext context ) => HomePage(),
+  };
+}
+}
